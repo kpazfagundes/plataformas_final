e526bf452f7bd9e17b498463f14049224db6269f
Karine Paz Fagundes Cordeiro <1082970@sga.pucminas.br>
2018-09-06 22:19:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+(function(angular) {
+  'use strict';
+angular.module('ngRouteExample', ['ngRoute'])
+ .controller('MainController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ })
+ .controller('BookController', function($scope, $routeParams) {
+     $scope.name = 'BookController';
+     $scope.params = $routeParams;
+ })
+ .controller('ChapterController', function($scope, $routeParams) {
+     $scope.name = 'ChapterController';
+     $scope.params = $routeParams;
+ })
+.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+   .when('/Book/:bookId', {
+    templateUrl: 'book.html',
+    controller: 'BookController',
+  })
+  .when('/Book/:bookId/ch/:chapterId', {
+    templateUrl: 'chapter.html',
+    controller: 'ChapterController'
+  });
+  $locationProvider.html5Mode(true);
+});
+})(window.angular);
